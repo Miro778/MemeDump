23d5aa344cc0849f1e42c8cb51ad3e9ffc2c3781
Miro778 <83011341+Miro778@users.noreply.github.com>
2021-09-24 14:53:26 +0300

    Korjattu ladattujen kuvatiedostojen korruptoituminen
    Selvitetty ongelmaa, jonka vuoksi ladatut tiedostot korruptoituvat. Muutettu backendiä, ja saatu ongelma korjattua.

--- backend/controllers/media.js
+++ backend/controllers/media.js
@@ -1,7 +1,9 @@
+/* eslint-disable no-useless-escape */
 /* eslint-disable no-unused-vars */
 var formidable = require('formidable')
 const mediaRouter = require('express').Router()
 const util = require('util')
+const fs = require('fs')
 
 mediaRouter.post('/', function(request, response, next) {
 
@@ -9,19 +11,39 @@ mediaRouter.post('/', function(request, response, next) {
 
   form.parse(request, function(err, fields, files) {
     if (err) {
-
-      // Check for and handle any errors here.
-
       console.error(err.message)
       return
     }
-    response.writeHead(200, { 'content-type': 'text/plain' })
-    response.write('received upload:\n\n')
+    const file = files.avatar
+    const isValid = isFileValid(file)
 
-    response.end(util.inspect({ fields: fields, files: files }))
+    if (!isValid) {
+      return response.status(400).json({
+        status: 'Fail',
+        message: 'The file type is not a valid type',
+      })
+    }
+
+    var oldpath = file.path
+    var newpath = 'C:/Users/Miro/OneDrive/Documents/Github/Memedump/backend/Images/' + file.name
+
+    fs.rename(oldpath, newpath, function (err) {
+      if (err) throw err
+      response.write('File uploaded and moved!')
+      response.end()
+    })
 
   })
 
 })
+
+const isFileValid = (file) => {
+  const type = file.type.split('/').pop()
+  const validTypes = ['jpg', 'jpeg', 'png', 'pdf']
+  if (validTypes.indexOf(type) === -1) {
+    return false
+  }
+  return true
+}
 
 module.exports = mediaRouter
